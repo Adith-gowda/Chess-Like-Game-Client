fd9eba22b45188b90bcd170bbd51609a7f9f9422
Adith sagar gowda <adithgowda06@gmail.com>
2024-08-27 19:22:02 +0530

    small bug fix

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import io from 'socket.io-client';
 import './App.css';
 
-const socket = io('http://localhost:3001');
+const socket = io('https://chess-like-game-server.onrender.com/');
 
 const DEFAULT_CHARACTERS = {
     A: ['P1', 'P2', 'H1', 'H2', 'P3'],
